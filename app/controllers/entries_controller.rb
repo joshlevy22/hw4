1345e4c5305e49f3f31379baff03529b2167750f
Josh Levy <josh.levy@kellogg.northwestern.edu>
2025-03-11 21:18:28 +0000

    did alot of code

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,12 +4,18 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
+    @user = User.find_by({"id" => session["user_id"]})
+    if @user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = @user["id"]
+      @entry.save
+    else
+      flash["notice"] = "Login first."
+    end
     redirect_to "/places/#{@entry["place_id"]}"
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "places", :action => "index" })
+  # Resources
   resources "entries"
   resources "places"
   resources "sessions"
   resources "users"
+
+  # Login / Logout
+  get("/login", { :controller => "sessions", :action => "new" })
+  get("/logout", { :controller => "sessions", :action => "destroy" })
+
+  # Define the root route
+  get("/", { :controller => "places", :action => "index" })
 end
