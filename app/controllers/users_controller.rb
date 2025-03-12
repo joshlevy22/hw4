efc593c1031dc93c4fea31b0997ce11e3f5ccc7b
Josh Levy <josh.levy@kellogg.northwestern.edu>
2025-03-12 19:01:46 +0000

    after you create an account, you go to login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,6 @@ class UsersController < ApplicationController
     @user["email"] = params["email"]
     @user["password"] = BCrypt::Password.create(params["password"])
     @user.save
-    redirect_to "/places"
+    redirect_to "/login"
   end
 end
